25eac15a2d72a1a7a1aa60b96dbf28ab62736952
avel moscare <avelqmoscare@gmail.com>
2018-10-06 17:49:19 +0800

    unresolve problem

---
+++ app/src/main/java/Textfield_Demo.java
@@ -0,0 +1,15 @@
+package com.fragments.drawer.moscare.drawer_fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Textfield_Demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_field_demo);
+
+        getSupportActionBar().setTitle("Text Field Demo");
+    }
+}
